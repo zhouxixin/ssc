83c2151858ef3534f724a93689483449e60a1e52
Zhou <zhouxixin@gmail.com>
2012-09-04 00:20:13 +1000

    add validation for orientation north + west = 100%

--- SimpleSolarCalculator/war/scripts/inputValidation.js
+++ SimpleSolarCalculator/war/scripts/inputValidation.js
@@ -70,9 +70,12 @@ function formValidation() {
 	if (!isValidDecimal(dataInputForm.systemSize)) numberOfErrors += 1;
 	if (!isValidPercentage(dataInputForm.panelEfficiency)) numberOfErrors += 1;
 	
-	//if (!isValidDecimal(dataInputForm.panelEfficiency))	numberOfErrors += 1;	
+		
 	if (!isValidPercentage(dataInputForm.percentageOnNorthRoof)) numberOfErrors += 1;
 	if (!isValidPercentage(dataInputForm.percentageOnWestRoof)) numberOfErrors += 1;
+	
+	if (!isValidOrientationSet(dataInputForm.percentageOnNorthRoof, dataInputForm.percentageOnWestRoof)) numberOfErrors += 1;
+	
 	if (!isValidPercentage(dataInputForm.efficiencyLossNorthRoof))	numberOfErrors += 1;
 	if (!isValidPercentage(dataInputForm.efficiencyLossWestRoof)) numberOfErrors += 1;
 	if (!isValidPercentage(dataInputForm.inverterEfficiency)) numberOfErrors += 1;
@@ -135,6 +138,20 @@ function isValidPercentage(fieldName) {
 		document.getElementById("info").style.color = "#E2041B";
 		return false;
 	}
+}
+
+function isValidOrientationSet(fieldName1, fieldName2) {
+	var sum = Number(fieldName1.value) + Number(fieldName2.value);
+	if (sum == 100.0) {
+		return true;
+	} else {
+		fieldName1.style.border = "thin solid #E2041B";
+		fieldName2.style.border = "thin solid #E2041B";
+		document.getElementById("info").innerHTML = "Please check for errors";
+		document.getElementById("info").style.color = "#E2041B";			
+		return false;
+	}
+	
 }
 
 function resetTable() {
